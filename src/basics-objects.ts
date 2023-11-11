ddb6b9360d881e5a71a36f1921ddb54a60952d15
J.c <Jak-Ch-ll@mailbox.org>
2023-11-11 11:54:05 +0100

    🚧 allow tracking of multiple objects

--- src/basics-objects.ts
+++ src/basics-objects.ts
@@ -1,16 +1,56 @@
-let product = { price: 5, quantity: 2 }
+// ### preparation ###
+type TargetMap = WeakMap<object, DependancyMap>
+type DependancyMap = Map<string | number | symbol, EffectSet>
+type EffectSet = Set<() => void>
+type Effect = () => void
+
+const targetMap: TargetMap = new WeakMap()
+
+// ### first object ###
+const product = { price: 5, quantity: 2 }
 let total = 0
 
-const effect = () => (total = product.price * product.quantity)
+const productEffect = () => (total = product.price * product.quantity)
 
-track('quantity')
-effect()
+track(product, 'quantity', productEffect)
+productEffect()
 
-const depsMap = new Map<string | number | symbol, Set<() => void>>()
+console.log('total', total) // 10
 
-function track(key: string | number | symbol) {
+// update the quantity
+product.quantity = 3
+// run all effects saved
+trigger(product, 'quantity')
+
+console.log('total', total) // 15
+
+// ### second object ###
+const user = { firstname: 'John', lastname: 'Doe' }
+let fullname = ''
+
+const userEffect = () => (fullname = `${user.firstname} ${user.lastname}`)
+
+track(user, 'firstname', userEffect)
+userEffect()
+
+console.log('fullname', fullname) // John Doe
+
+// update the firstname
+user.firstname = 'Jane'
+// run all effects saved
+trigger(user, 'firstname')
+
+console.log('fullname', fullname) // Jane Doe
+
+// ### implementation ###
+function track(target: object, key: string | number | symbol, effect: Effect) {
+	let depsMap = targetMap.get(target)
+	if (!depsMap) {
+		depsMap = new Map()
+		targetMap.set(target, depsMap)
+	}
+
 	let dep = depsMap.get(key)
-
 	if (!dep) {
 		dep = new Set()
 		depsMap.set(key, dep)
@@ -19,20 +59,12 @@ function track(key: string | number | symbol) {
 	dep.add(effect)
 }
 
-function trigger(key: string | number | symbol) {
-	let dep = depsMap.get(key)
+function trigger(target: object, key: string | number | symbol) {
+	let depsMap = targetMap.get(target)
+	let dep = depsMap?.get(key)
 	if (dep) {
 		dep.forEach((effect) => effect())
 	}
 }
-
-console.log('total', total) // 10
-
-// update the quantity
-product.quantity = 3
-// run all effects saved
-trigger('quantity')
-
-console.log('total', total) // 15
 
 export {}
